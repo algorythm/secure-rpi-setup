53059a26b944579297179a6f271fb35ddaaab1dd
algorythm <anders@wiberg.tech>
2018-09-15 20:18:12 +0200

    cd to correct dir

--- download-install.sh
+++ download-install.sh
@@ -8,5 +8,5 @@ fi
 wget https://github.com/algorythm/secure-rpi-setup/archive/master.zip
 [ ! -x "$(command -v unzip)" ] && sudo apt-get install -y unzip
 unzip master.zip
-cd secure-rpi-setup
+cd secure-rpi-setup*
 sudo ./setup.sh
