a94302b686e8bb5560b81f9b652b6fc255c087fa
algorythm <anders@wiberg.tech>
2018-09-15 20:38:45 +0200

    Don't check for root in download-insatll.sh

--- download-install.sh
+++ download-install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-if [ ! $EUID -eq 0 ]; then
-    echo "Please run as sudo.\n" >&2
-    exit 1
-fi
-
 wget https://github.com/algorythm/secure-rpi-setup/archive/master.zip
 [ ! -x "$(command -v unzip)" ] && sudo apt-get install -y unzip
 unzip master.zip
